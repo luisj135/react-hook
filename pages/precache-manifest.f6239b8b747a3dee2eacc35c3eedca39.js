36ccfbd53a6430746499df8775168ddbdbc29548
luis jhoham venegas tobar <lvenegas@factolabs.com>
2019-04-22 11:21:31 -0500

    include build files

---
+++ pages/precache-manifest.f6239b8b747a3dee2eacc35c3eedca39.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c987a056fc15940eb98c",
+    "url": "/static/js/main.69fe493c.chunk.js"
+  },
+  {
+    "revision": "0548a7b5e383a1f0e42e",
+    "url": "/static/js/2.698453ec.chunk.js"
+  },
+  {
+    "revision": "c987a056fc15940eb98c",
+    "url": "/static/css/main.d0d7ab09.chunk.css"
+  },
+  {
+    "revision": "92d9bdb86b21ca6b29f05acf321bcee0",
+    "url": "/index.html"
+  }
+];
